88e02b27acf4b7bbbe987919491940bc6a0125dd
Engineerrani <90748634+Engineerrani@users.noreply.github.com>
2021-09-19 10:27:47 +0530

    Add files via upload

---
+++ sumnumber.java
@@ -0,0 +1,10 @@
+public class sumnumber {
+  public static void main(String[] args) {
+      int i, num = 10, sum= 0;
+      for(i=1; i<=num; ++i)
+      {
+          sum = sum + i;
+      }
+ System.out.println("Sum of First 10 number is="+sum);
+  }
+}
