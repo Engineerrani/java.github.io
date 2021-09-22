09c30f22bac192c0b795aa21133660faf7b1897a
Rani Maurya <90748634+Engineerrani@users.noreply.github.com>
2021-09-22 18:07:54 +0530

    Add files via upload
    nice

---
+++ FabinacciSeq.java
@@ -0,0 +1,34 @@
+package javaprg;
+import java.util.Scanner;
+
+public class FabinacciSeq {
+	public static void main(String[] args) {
+		//1,1,2,3,5,8,13,21,34
+		int a=0;
+		int b=1;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int count=scan.nextInt();
+		if(count==0) {
+			System.out.print(a);
+		}
+		else if(count==1) {
+			System.out.print(a+",");
+			System.out.print(b+",");
+		}
+	else {
+		System.out.print(a+",");
+		System.out.print(b+",");
+		
+		while(count>=0) {
+			int fibo=a+b;
+			System.out.print(fibo+",");
+			a=b;
+			b=fibo;
+			count=count-1;
+		}
+	}
+	scan.close();
+
+}
+}
